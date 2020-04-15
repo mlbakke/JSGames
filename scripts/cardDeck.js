26304de2ca3d339b8e6d7b112b10ce81c316d598
Martin Bakke <bakkemartin11@gmail.com>
2020-04-15 15:23:10 +0200

    Deck of cards
    created deck object with fill, shuffle and draw methods

---
+++ scripts/cardDeck.js
@@ -0,0 +1,30 @@
+const cardDeck = {
+	deck        : [],
+	suits       : [ 'hearts', 'diamonds', 'spades', 'clubs' ],
+	values      : '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+	fillDeck() {
+		const { suits, values, deck } = this;
+		//push one of each card into deck
+		for (let value of values.split(',')) {
+			for (let suit of suits) {
+				deck.push({
+					value,
+					suit
+				});
+			}
+		}
+	},
+	shuffleDeck() {
+		const { deck } = this;
+		//Durstenfeld shuffle -- O(n)
+		for (let i = deck.length - 1; i > 0; i--) {
+			// const j: 0 <= j <= i
+			const j = Math.floor(Math.random() * (i + 1));
+			//swap card [i] and [j]
+			[ deck[i], deck[j] ] = [ deck[j], deck[i] ];
+		}
+	},
+	drawCard() {
+		return this.deck.pop();
+	}
+};
