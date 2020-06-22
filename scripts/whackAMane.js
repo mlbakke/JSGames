5a1bf91f82226f9f5ada9888da974951f2109de1
Martin Bakke <bakkemartin11@gmail.com>
2020-06-22 14:04:45 +0200

    outline of script

--- scripts/whackAMane.js
+++ scripts/whackAMane.js
@@ -1,28 +1,30 @@
-const score = document.querySelector("#molescore");
-const holes = document.querySelectorAll(".hole");
-const moles = document.querySelectorAll(".mole");
-const start = document.querySelector("#newGame");
+const score = document.querySelector('#molescore');
+const holes = document.querySelectorAll('.hole');
+const moles = document.querySelectorAll('.mole');
+const start = document.querySelector('#newGame');
 let currScore = 0;
 let finished = false;
 
-start.addEventListener("click", function() {
-    score.textContent = 0;
-    finished = false;
-    currScore = 0;
-    //start moles/manes to appear
+start.addEventListener('click', function() {
+	score.textContent = 0;
+	finished = false;
+	currScore = 0;
+	//start moles/manes to appear
 
-    //setTimeout(() => finished = true, xxxx ms)
+	//setTimeout(() => finished = true, xxxx ms)
 });
 
-function appear() {
-    //const time = make moles appear at a somewhat random time
-    //const hole = pick hole to appear
-
-    //make mole appear and disappear after a certain amount of time
+function getTime(min, max) {
+	Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function hit {
-    //remove mole
+function appear() {
+	//const time = make moles appear at a somewhat random time
+	//const hole = pick hole to appear
+	//make mole appear and disappear after a certain amount of time
+}
 
-    //update score
+function hit() {
+	//remove mole
+	//update score
 }
